713b98b43a838ef462f41b003d2a4a74eb1bb2d7
Perkles <luiz.antonio.da.silva777@gmail.com>
2022-10-08 15:54:18 -0300

    ADICAO DE MENU

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+int verificador=0;
+
+while(verificador!=1)
+{
+    
+    Console.WriteLine("TECLE 1 PARA SAIR ");
+    Console.WriteLine("TECLE 2 PARA SOMAR ");
+    Console.WriteLine("TECLE 3 PARA SUBTRAIR ");
+    Console.WriteLine("TECLE 4 PARA MULTIPLICAR ");
+    Console.WriteLine("TECLE 5 PARA DIVIDIR ");
+    
+    verificador=int.Parse(Console.ReadLine());
+    
+}
+Console.WriteLine("O PROGRAMA VAI SER ENCERRADO !!!");
